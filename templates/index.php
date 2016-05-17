b42b123782114e4d91ee824463981b66113465a6
BrendanMurray <thebmurr@gmail.com>
2016-05-17 16:08:18 -0400

    added more static watches and fixed staying

--- templates/index.php
+++ templates/index.php
@@ -6,28 +6,70 @@
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
  </head>
 	 <body class="bg">
-	 	<div class="container text-center"></br></br>
-	 		<h2 style='text-align:center; vertical-align: middle; color:black;'>Optimal watch list under construction.</h2>
+	 	<div class="container text-center">
+	 		<h2 style='text-align:center; vertical-align: middle; color:black;'>Watches to Watch</h2>
             <ul class="products">
                 <li>
                     <a href="#">
-                        <img src="/static/images/SNK809.jpg" width=200px>
+                        <img src="/static/images/SNK809.jpg" width=225px>
                         <h4>Seiko SNK809</h4>
                         <p>$58.65</p>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <img src="/static/images/SNZF15J.jpg" width=200px>
+                        <img src="/static/images/SNZF15J.jpg" width=225px>
                         <h4>Seiko SNZF15J</h4>
                         <p>$225.00</p>
                     </a>
                 </li>
                 <li>
                     <a href="http://www.amazon.com/Seiko-Mechaical-Alpinist-Automatic-Sarb017/dp/B000KG93BQ/">
-                        <img src="/static/images/SARB017.jpg" width=200px>
+                        <img src="/static/images/SARB017.jpg" width=225px>
                         <h4>Seiko SARB017</h4>
                         <p>$375.00</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.amazon.com/Seiko-SKX007K-Divers-Automatic-Watch/dp/B000B5MI3Q/">
+                        <img src="/static/images/SKX007Kzoom.jpg" width=225px>
+                        <h4>Seiko SKX007K</h4>
+                        <p>$216.80</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.amazon.com/Seiko-SSB025-Chronograph-stainless-Steel/dp/B007554WYI/">
+                        <img src="/static/images/SSB025.jpg" width=225px>
+                        <h4>Seiko SSB025</h4>
+                        <p>$138.00</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.amazon.com/Seiko-MECHANICAL-SARB033-Wrist-Watch/dp/B001AE9XRQ/">
+                        <img src="/static/images/SARB033.jpg" height=225px>
+                        <h4>Seiko SARB033</h4>
+                        <p>$343.00</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.amazon.com/gp/product/B0038OLUMM/">
+                        <img src="/static/images/SARB065.jpg" height=225px>
+                        <h4>Seiko SARB065</h4>
+                        <p>$405.00</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.amazon.com/Casio-Black-Analog-Reverse-Bezel/dp/B01C4P60J8/">
+                        <img src="/static/images/MDV106-1A.jpg" height=225px>
+                        <h4>Casio MDV106-1A</h4>
+                        <p>$40.99</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.amazon.com/Orient-FER24004B0-Bambino-Stainless-Automatic/dp/B00FS43D7A/">
+                        <img src="/static/images/FER24004B0.jpg" height=225px>
+                        <h4>Orient FER24004B0</h4>
+                        <p>$155.95</p>
                     </a>
                 </li>
             </ul>
