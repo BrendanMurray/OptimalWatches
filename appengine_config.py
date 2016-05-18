c94be9276bdcec714b055071daee5c425d8b8fb6
BrendanMurray <thebmurr@gmail.com>
2016-05-18 14:37:21 -0400

    accessing api info working

--- appengine_config.py
+++ appengine_config.py
@@ -1,4 +1,6 @@
 from google.appengine.ext import vendor
+import os
 
 # Add any libraries installed in the "lib" folder.
-vendor.add('lib')
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+#vendor.add('lib')
--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import webapp2
 import models
 import time
 import json
+import sys
 
 from google.appengine.api import mail
 from google.appengine.ext import ndb
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import template
+sys.path.insert(0, 'libs')
+from lxml import objectify
+from amazon.api import AmazonAPI
 
 ###############################################################################
 # We'll just use this convenience function to retrieve and render a template.
@@ -22,6 +26,10 @@ def render_template(handler, templatename, templatevalues={}):
 #this class renders the homepage
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
+        amazon = AmazonAPI()
+        product = amazon.lookup(ItemId='B000KG93BQ')
+        logging.warning(product.title)
+        logging.warning(product.price_and_currency[0])
         render_template(self, 'index.html')
 
 ###############################################################################
